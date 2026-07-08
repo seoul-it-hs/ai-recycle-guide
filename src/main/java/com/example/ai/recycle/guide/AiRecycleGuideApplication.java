02981cc57ae877d75682660412ab2e1d5494b513
종훈 <jonghoon023@gmail.com>
2026-07-08 13:27:02 +0900

    Initialize Spring Boot project

---
+++ src/main/java/com/example/ai/recycle/guide/AiRecycleGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai.recycle.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecycleGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRecycleGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai/recycle/guide/AiRecycleGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai.recycle.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecycleGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
